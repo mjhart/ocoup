16fe8cb0a10cc93958962f4e284a1df47ea278c4
Matthew Hart <matthart4@gmail.com>
2025-11-22 16:27:17 -0500

    minor interface change

--- server/lib/player_ios.mli
+++ server/lib/player_ios.mli
@@ -1,12 +1,12 @@
 open! Core
 open! Async
 open Types
+include Player_io_S
 
-type t
+val create : (module Player_io_S with type t = 'a) -> 'a -> t
+
+(* Some useful player_io impls *)
 
 val llm : Player_id.t -> model:string -> t Deferred.t
 val cli : Player_id.t -> t Deferred.t
 val gemini : Player_id.t -> model:string -> t Deferred.t
-val create : (module Player_io_S with type t = 'a) -> 'a -> t
-
-include Player_io_S with type t := t
